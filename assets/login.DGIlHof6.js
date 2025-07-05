a725c4dc0e91d99e3165d5666b53e582f5c41782
moling3650 <365024424@qq.com>
2025-07-05 23:41:12 +0800

    feat: 宠物定位器 v0.0.6

---
+++ assets/login.DGIlHof6.js
@@ -0,0 +1,1 @@
+import"./index-Bj0IGgE5.js";import{h as s}from"./http.DZwPGMfx.js";const e=e=>s.post("/oauth/token",{},e),t=e=>s.post("/pet/query",e),o=e=>s.post("/pet/bind",e),p=e=>s.post("/pet/update",e),a=e=>s.post(`/pet/delete?id=${e}`),r=s=>Promise.resolve({code:0,message:""}),d=s=>Promise.resolve({code:0,message:""});export{e as a,o as b,a as c,t as g,r as s,p as u,d as v};
---
+++ assets/useSafeAreaInsets.Q6GV8cbF.js
@@ -0,0 +1,1 @@
+import{A as e}from"./index-Bj0IGgE5.js";function s(){let s,n=null;return s=e(),n=s.safeAreaInsets,{safeAreaInsets:n}}export{s as u};
